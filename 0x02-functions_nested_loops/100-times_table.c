0af8f3828b45afd26aadfbe522aad837fef01856
3nzio <eezy911@gmail.com>
2023-06-20 12:43:11 -0700

    0x02-functions_nested_loops Advanced Tasks 12 Fixed

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,31 +6,43 @@
  */
 void print_times_table(int n)
 {
-    if (n < 0 || n > 15)
-        return;
+	if (n >= 0 && n <= 15)
+	{
+		for (int i = 0; i <= n; i++)
+		{
+			for (int j = 0; j <= n; j++)
+			{
+				int k = j * i;
 
-    for (int i = 0; i <= n; i++)
-    {
-        for (int j = 0; j <= n; j++)
-        {
-            int k = j * i;
-            if (j != 0)
-                _putchar(',');
-
-            _putchar(' ');
-
-            if (k < 10)
-                _putchar(' ');
-
-            if (k >= 100)
-                _putchar((k / 100) + '0');
-
-            if (k >= 10)
-                _putchar(((k / 10) % 10) + '0');
-
-            _putchar((k % 10) + '0');
-        }
-        _putchar('\n');
-    }
+				if (j == 0)
+					_putchar(k + '0');
+				else if (k < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				}
+				else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+				else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
 
