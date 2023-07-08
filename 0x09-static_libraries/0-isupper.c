8a25fa3dc3acfe8dc3afca00ab7a86424a0ec30f
3nzio <eezy911@gmail.com>
2023-07-07 18:07:10 -0700

    0x09. C - Static libraries Tasks Done & Errors Fixed

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,12 +1,10 @@
-0-isupper.c
-
 #include "main.h"
 
 /**
-* _isupper - uppercase letters
-* @c: char to check
+*_isupper - uppercase letters
+*@c: char to check
 *
-* Return: 0 or 1
+*Return: 0 or 1
 */
 
 int _isupper(int c)
