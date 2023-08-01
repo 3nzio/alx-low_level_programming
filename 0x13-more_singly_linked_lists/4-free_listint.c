0651aade098ff19219c88b944a361662d4aaf1d1
“3nzio” <eezy911@gmail.com>
2023-08-01 10:50:42 -0700

    Fixes Made

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,18 +1,18 @@
 #include "lists.h"
 
 /**
- * free_listint – Will free a linked list
+ * free_listint - Frees a linked list
  * @head: listint_t list to be freed
  */
 void free_listint(listint_t *head)
 {
- listint_t *temp;
+	listint_t *temp;
 
- while (head)
- {
- temp = head->next;
- free(head);
- head = temp;
- }
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
 }
 
--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,23 +1,23 @@
 #include "lists.h"
 
 /**
- * free_listint2 – Will free a linked list
+ * free_listint2 - Frees a linked list
  * @head: pointer to the listint_t list to be freed
  */
 void free_listint2(listint_t **head)
 {
- listint_t *temp;
+	listint_t *temp;
 
- if (head == NULL)
- return;
+	if (head == NULL)
+		return;
 
- while (*head)
- {
- temp = (*head)->next;
- free(*head);
- *head = temp;
- }
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
 
- *head = NULL;
+	*head = NULL;
 }
 
